696c477af587ca9ba2f53deb405f76e3a260e795
ariunalapointe <ariuna.lapointe@gmail.com>
2025-11-10 20:33:16 -0500

    added comments

---
+++ src/main/java/org/example/practice_Day1.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class practice_Day1 {
+    static void main(String[] args) {
+        System.out.println("Hello world");
+
+        //added some comments here
+    }
+}
