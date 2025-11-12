f4e82b51cd155941bf2a7198e139850e8538319b
ariunalapointe <ariuna.lapointe@gmail.com>
2025-11-11 19:31:01 -0500

    delete some comments

--- src/main/java/org/example/AuthorisationPage.java
+++ src/main/java/org/example/AuthorisationPage.java
@@ -4,8 +4,6 @@ public class AuthorisationPage {
     static void main(String[] args) {
         System.out.println("This is a new authorisation page");
         System.out.println("This is a new authorisation page");
-        System.out.println("This is a new authorisation page");
-        System.out.println("This is a new authorisation page");
-        System.out.println("This is a new authorisation page");
+
     }
 }
