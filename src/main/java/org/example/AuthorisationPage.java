37946b135662e6cef8ccc112779b4b022d8cbff8
ariunalapointe <ariuna.lapointe@gmail.com>
2025-11-11 21:48:25 -0500

    add extra test cases

--- src/main/java/org/example/AuthorisationPage.java
+++ src/main/java/org/example/AuthorisationPage.java
@@ -4,6 +4,7 @@ public class AuthorisationPage {
     static void main(String[] args) {
         System.out.println("This is a new authorisation page");
         System.out.println("This is a new authorisation page");
+        System.out.println("This is a new authorisation page");
 
     }
 }
